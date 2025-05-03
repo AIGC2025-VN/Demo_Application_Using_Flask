b3b96294c4d006b99b41a942eaf9c9898e003369
AndrewVu-VuGiaTue <vgt2800@gmail.com>
2025-05-03 17:02:01 +0700

    Add files via upload

---
+++ demo_app.py
@@ -0,0 +1,11 @@
+# demo_app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World! This is my very first Flask app!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
